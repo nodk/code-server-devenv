ea9b5fcfc3fc5cf5e8b9d5c2a4a50fddb99817d7
nigodk <nigodk@gmail.com>
2019-04-17 00:53:48 +0900

    Refine Dockerfile, remove lang_dir arg from docker-compose build.

--- common/scripts/install-extensions.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-grep -v -e '^\s*#' -e '^\s*$' $1 | awk '{system("code --user-data-dir --extensions-dir /tmp/.code-server/extensions --install-extension "$1)}'
-if [ -e "$2" ] && [ -d "$2" ]; then
-  ls $2/*.vsix | awk '{system("code --user-data-dir --extensions-dir /tmp/.code-server/extensions --install-extension "$1)}'
-fi
